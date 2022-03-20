3ea81033a66cbb1f765f6cba172802f5897ba1e8
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-03-20 13:16:51 -0300

    feat: work aula-03

---
+++ src/FunctionalInterfaceMain.java
@@ -0,0 +1,31 @@
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionalInterfaceMain {
+    public static void main(String[] args) {
+
+        //Faz um teste qualquer no objeto recebido, retorna [TRUE] [FALSE]
+        Predicate<String> predicate = s -> s.length() > 4;
+        System.out.println("[Predicate] " + predicate.test("teste"));
+
+        //Não recebe nenhum valor e retorna um resultado T, bom para instanciar objetos
+        Supplier<Object> objectSupplier = () -> new Object();
+        System.out.println("[Supplier] " + objectSupplier.get().getClass().getSimpleName());
+
+        //Recebe um valor T e retorna R, bom para fazer processamento de informação
+        Function<Object, String> objectStringFunction = object -> object.getClass().getSimpleName();
+        System.out.println("[Function] " + objectStringFunction.apply(new Object()));
+
+        //Recebe valor T e imprime
+        Consumer<Object> objectConsumer = object -> System.out.println("[Consumer] " + object.getClass().getSimpleName());
+        objectConsumer.accept(new Object());
+
+        //Compara dois valores e retorna um int
+        Comparator<Object> objectComparator = (object1, object2) -> object1.toString().compareTo(object2.toString());
+        System.out.println("[Comparator] " + objectComparator.compare(new Object(), new Object()));
+    }
+}
